eb54eaf31f94ec22ffe1c8949e205fa769d7d38e
Blake Runyon <blakerunyon@Blakes-MacBook-Pro.local>
2020-06-10 15:49:37 -0600

    V1 Unit 1 - Button Loads Quotes

--- Unit 1/js/script.js
+++ Unit 1/js/script.js
@@ -3,27 +3,56 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Quotes Array
 
-/*** 
- * `quotes` array 
-***/
+var quotes = [
+  {
+    quote: "We are all responsible for everyone else—but I am more responsible than all the others.",
+    source: "Fyodor Dostoyevsky",
+    year: "1880"
+  },
+  {
+    quote: "Every man in a bank hates what the bank does, and yet the bank does it. The bank is something more than men, it's the monster. Men made it, but they can't control it.",
+    source: "John Steinbeck",
+    year: "1939"
+  },  
+  {
+    quote: "There is only one thing that I dread: not to be worthy of my sufferings.",
+    source: "Fyodor Dostoyevsky",
+    year: "Circa 1870"
+  },
+  {
+    quote: "There is nothing noble in being superior to your fellow man; true nobility is being superior to your former self.",
+    source: "Ernest Hemingway",
+    year: "1922"
+  },
+  {
+    quote: "In any moment of decision, the best thing you can do is the right thing. The worst thing you can do is nothing.",
+    source: "Teddy Roosevelt",
+    year: "1907"
+  }
+]
 
+// Random Number Function
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
-/***
- * `getRandomQuote` function
-***/
+// Create Random Number
 
-
+// var quoteNum = getRandomInt(quotes.length);
 
 /***
  * `printQuote` function
 ***/
 
-
+function printQuote() {
+  var quoteNum = getRandomInt(quotes.length);
+  document.getElementsByClassName('quote')[0].innerText = quotes[quoteNum]["quote"]
+  document.getElementsByClassName('source')[0].innerText = quotes[quoteNum]["source"]
+  document.getElementsByClassName('year')[0].innerText = quotes[quoteNum]["year"]
+}
 
 /***
  * click event listener for the print quote button
